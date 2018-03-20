43d6ee2b3d859e51d79a6208ff1b0526adc2c0f9
Anais Kim <itschstyle@gmail.com>
2018-03-21 08:13:42 +0900

    initial commit - add simple html, css - add webpack, webpack-dev-server, babel

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Profile } from './Profile'
+
+const pf = new Profile('Anais Kim', 'itschstyle@gmail.com')
+console.log(pf.hello())
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname),
+                exclude: /(node_modules)|(dist)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+};
