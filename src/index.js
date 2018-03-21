c5a36e7752661c8bfef329578af295ce74b43c41
Anais Kim <itschstyle@gmail.com>
2018-03-21 14:43:05 +0900

    add simple fetchBooks() function to load book information - use google book open api to get book resources - add more css design to show book list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-import { Profile } from './Profile'
+function fetchBooks() {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=javascript').then(res => {
+        if (res.ok) {
+            res.json().then((data) => {
+                console.log(data.items);
 
-const pf = new Profile('Anais Kim', 'itschstyle@gmail.com')
-console.log(pf.hello())
+                const books = document.getElementById('books');
+                books.innerHTML = '';
+
+                for (const book of data.items) {
+                    const {imageLinks: {thumbnail}, title, authors, description} = book.volumeInfo;
+
+                    books.innerHTML += `<li>
+                            <img src="${thumbnail}"/>
+                            <div class="book-detail">
+                                <p class="book-title">${title}</p>
+                                <p class="book-authors">${authors}</p>
+                                <p class="book-description">${(description)? description : ''}</p>
+                            </div>
+                            <div class="bookmark-icons">
+                                 <i class="far fa-star"></i>
+                                 <!--<i class="fa fa-star"></i>-->
+                            </div>
+                            </li>`;
+                }
+            });
+        } else {
+            console.log('Cannot get book resources from Google Books.');
+        }
+    });
+}
+
+window.fetchBooks = fetchBooks;
