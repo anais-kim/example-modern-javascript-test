88c0e22819ce4ce1d0bbd95da30b2ee41e00a596
Anais Kim <itschstyle@gmail.com>
2018-03-27 10:30:30 +0900

    refactor test codes - use sinon clearly (use spy rather than stub)

--- test/services/google-book-service-spec.js
+++ test/services/google-book-service-spec.js
@@ -4,19 +4,11 @@ import {expect} from 'chai';
 import * as GoogleBookService from '../../src/services/google-book-service'
 import * as ResponseHandler from '../../src/services/response-handler';
 
-require('babel-polyfill');
-
 describe('GoogleBookService', () => {
     describe('#fetchBooksBykeyword', () => {
-        let result;
-        const BOOKS = {items: ['book with keyword']};
-
-        beforeEach(async () => {
-            fetchMock.get('*', BOOKS);
-
-            sinon.stub(ResponseHandler, 'handleResponse').resolves(BOOKS);
-
-            result = await GoogleBookService.fetchBooksByKeyword('keyword');
+        beforeEach(() => {
+            fetchMock.get('*', {items: [{title: 'book1'}, {title: 'book2'}]});
+            sinon.spy(ResponseHandler, 'handleResponse');
         });
 
         afterEach(() => {
@@ -24,17 +16,23 @@ describe('GoogleBookService', () => {
             ResponseHandler.handleResponse.restore();
         });
 
-        it('should fetchs volume api with query parameters keyword', () => {
+        it('should fetch volume api with query parameter', async () => {
+            await GoogleBookService.fetchBooksByKeyword('keyword');
+
             const url = fetchMock.calls()[0][0];
             expect(url).to.equal('https://www.googleapis.com/books/v1/volumes?q=keyword');
         });
 
-        it('should call handleResponse with fetched response', () => {
+        it('should call handleResponse with fetched response', async () => {
+            await GoogleBookService.fetchBooksByKeyword('keyword');
+
             expect(ResponseHandler.handleResponse.calledWith(sinon.match.object)).to.be.true;
         });
 
-        it('should result returned', () => {
-            expect(result).to.deep.equal(BOOKS);
+        it('should return result', async () => {
+            const result = await GoogleBookService.fetchBooksByKeyword('keyword');
+
+            expect(result).to.deep.equal({items: [{title: 'book1'}, {title: 'book2'}]});
         });
     })
 })
